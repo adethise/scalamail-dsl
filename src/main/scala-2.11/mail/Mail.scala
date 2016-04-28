ed229f1a35120bf0d5cd5ce3d704ac88483f8ca1
Arnaud Dethise <arnauddethise@gmail.com>
2016-04-28 14:15:44 +0200

    added test image

--- src/main/scala-2.11/mail/Mail.scala
+++ src/main/scala-2.11/mail/Mail.scala
@@ -2,22 +2,24 @@ package mail
 
 class Mail extends MailTemplate {
 
-  host("localhost:2525")
+	host("localhost:2525")
 
-  "saber@fate.net" to("archer@fate.net", "rider@fate.net") cc "shirou@fate.net"
+	"saber@fate.net" to("archer@fate.net", "rider@fate.net") cc "shirou@fate.net"
 
-  message subject
-    "Holy Grail War"
+	message subject
+			"Holy Grail War"
 
-  message attachment
-          ("test.txt", "test2.txt", "siwj.png")
+	message attachment
+			("test.txt", "test2.txt", "image.png")
 
-  message content
-    """
-       <h1>A note about the war</h1>
+	message content
+			"""
+	  <h1>A note about the war</h1>
 
-       <p>Dear fellow heroes, everything is going as planned.<p>
-       <p>See you on the battlefield.</p>
-       <p>Saber</p>
-    """
+	  <p>Dear fellow heroes, everything is going as planned.<p>
+	  <p>See you on the battlefield.</p>
+
+	  <p>Saber</p>
+			"""
+
 }
